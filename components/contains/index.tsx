39bf9698afee804bb4ec2e2c49277661f0c46156
Khiet Tuong <ddmin4650@gmail.com>
2024-05-04 17:12:58 -0700

    uploat new coding

--- components/contains/index.tsx
+++ components/contains/index.tsx
@@ -3,17 +3,17 @@ import { FolderKanban, Home, UserRound } from "lucide-react";
 const SidebarLink = [
     {
         url:"/",
-        icons:<Home className='ml-4'/> ,
+        icons:<Home className='ml-4 text-'/> ,
         title:"Dashboard"
     },
     {
         url:"/project",
-        icons:<FolderKanban className='ml-4'/> ,
+        icons:<FolderKanban className='ml-4 text-green-500'/> ,
         title:"Project"
     }, 
     {
         url:"/profile",
-        icons:<UserRound className='ml-4'/> ,
+        icons:<UserRound className='ml-4 text-'/> ,
         title:"Profile"
     },
 
--- components/contains/sidebar.tsx
+++ components/contains/sidebar.tsx
@@ -12,8 +12,8 @@ const Sidebar = () => {
       {SidebarLink.map(link => (
 
         <Link href={link.url} className={twMerge("mt-5  flex items-center h-12 rounded-lg w-full",
-          "hover:bg-gray-50 hover:border-l-4 hover:border-blue-300 hover:shadow-lg hover:shadow-gray-900/5 ",
-          link.url === pathname ? "bg-gray-50 border-l-4 border-blue-500 shadow-lg shadow-gray-900/10 " : ""
+          "hover:bg-gray-50 hover:border-l-4 hover:border-blue-200 hover:shadow-lg hover:shadow-gray-900/5 ",
+          link.url === pathname ? "bg-gray-100 border-l-4 border-blue-500 shadow-lg shadow-gray-900/10 " : ""
         )} key={link.url}>
           {link.icons}
           <span className=' text-lg ml-3'>{link.title}</span>
